5310ea2af276509844be4fa5efe95952c89739ae
Le Wang <lewang.dev@gmail.com>
2021-11-26 11:21:40 +0800

    add nodejs as docker builder

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,5 @@
-docker build . -f Dockerfile-gcc-8.2.0-centos7 -t gcc:8.2.0-centos7
-docker build . -f Dockerfile-python-3.7-centos -t python:3.7-centos7
-docker build . -f Dockerfile-paddlepaddle-2.2.0-centos7 -t paddlepaddle:2.2.0-centos7
-docker build . -t paddlewebocr
+sudo docker build . -f Dockerfile-gcc-8.2.0-centos7 -t gcc:8.2.0-centos7
+sudo docker build . -f Dockerfile-python-3.7-centos -t python:3.7-centos7
+sudo docker build . -f Dockerfile-paddlepaddle-2.2.0-centos7 -t paddlepaddle:2.2.0-centos7
+sudo docker build . -f Dockerfile-nodejs -t nodejs
+sudo docker build . -t paddlewebocr
